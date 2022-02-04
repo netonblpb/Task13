2079b9b7af82a0b1819f3fcf528b642f7a7cf92c
netonblpb <skullbog@gmail.com>
2022-02-04 11:02:54 +0300

    Task13 complete

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Task13: Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int startNumber = 10;
+
+Console.Write("Введите число ");
+string checkNumber = Console.ReadLine();
+int intNumber = Convert.ToInt16(checkNumber);
+
+if (intNumber % startNumber != 0) 
+Console.WriteLine("Остаток от деления " + checkNumber + " на " + startNumber + " равен " + intNumber % startNumber);
